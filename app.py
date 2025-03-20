d723d21f093f0119a0358ce1bce936f00d0e68db
nikolp18 <npeneva08@gmail.com>
2025-03-20 13:37:51 +0200

    webforms and login stuff

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from Flask import db, login_manager
-from flask_login import UserMixin
+from flask import Flask, db, login_manager
+from flask_login import UserMixin, LoginManager
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import login_user, login_required, logout_user, current_user
+from flask import render_template, request, redirect, url_for, flash, session
+from webforms import LoginForm
 
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SECRET_KEY'] = "samo levski"
 
+db = SQLAlchemy()
 
 class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -45,3 +53,40 @@ class TimeOffRequest(db.Model):
     end_date = db.Column(db.Date, nullable=False)
     reason = db.Column(db.String(200))
     status = db.Column(db.String(20), default='Pending')
+
+with app.app_context():
+	db.create_all()
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+	return Users.query.get(int(user_id))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		username = request.form['username']
+		user = Users.query.filter_by(username=username).first()
+		if user:
+			if check_password_hash(user.password_hash, form.password.data):
+				login_user(user)
+				flash("Login Succesfull")
+				session['username'] = username
+				return redirect(url_for('dashboard'))
+			else:
+				flash("Wrong Password - Try Again")
+		else:
+			flash("User Not Found - Try Again")
+	return render_template('login.html', form=form)
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+	logout_user()
+	flash("You Have Been Logged Out")
+	return redirect(url_for('login'))
+
